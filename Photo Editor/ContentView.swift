8a5bb5bd82f8156935a2a0b223a5e05a58d62b29
sxc <qiuyequ@icloud.com>
2020-09-27 11:45:50 +0800

    Initial Commit

---
+++ Photo Editor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Photo Editor
+//
+//  Created by Xiaochun Shen on 2020/9/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Photo Editor/Photo_EditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Photo_EditorApp.swift
+//  Photo Editor
+//
+//  Created by Xiaochun Shen on 2020/9/27.
+//
+
+import SwiftUI
+
+@main
+struct Photo_EditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
